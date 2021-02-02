9aa68dc3d6e514f20e3af302830f9df545457a2f
UMUTONIRitha <rithamarie9@gmail.com>
2021-02-02 08:21:42 +0200

    add changes

--- hood/models.py
+++ hood/models.py
@@ -5,6 +5,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Neighbourhood(models.Model):
+    
     hood_name = models.CharField(max_length=60)
     location = models.CharField(max_length=60)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
